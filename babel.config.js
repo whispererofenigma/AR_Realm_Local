a608ce550cbc9f58b492b7516453d2f942db4f45
Tuhin Das <dastuhin.rnj@gmail.com>
2026-02-13 11:15:56 +0530

    Project Initialisation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+ const workletsPluginOptions = {
+    // Your custom options.
+  }
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+   plugins: [
+    ['module-resolver', {
+      root: ['./App'],
+      alias: {
+        '@': './App'
+      }
+    }],
+    ['react-native-worklets/plugin', workletsPluginOptions],
+  ]
 };
---
+++ react-native.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  assets: ['./App/assets/fonts', './node_modules/react-native-vector-icons/Fonts'],
+};
